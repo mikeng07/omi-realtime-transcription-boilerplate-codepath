bf98d8beda0f9eecbbc7c7bdf4f5c67fb6b2a66f
Mike <nguyentung2107@gmail.com>
2025-11-20 15:28:21 -0800

    complete setting up webhook

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app.add_middleware(
 
 @app.post("/webhook")
 def webhook(uid: str, transcript: dict):
-    print(transcript)
+    
 
     # Hint: The transcript contains segments with text data
     # Hint: Access the latest segment with transcript["segments"][-1]["text"]
@@ -25,8 +25,19 @@ def webhook(uid: str, transcript: dict):
     # example: if the word "tired" is mentioned, return a message notifying the user to take a break
 
     # TODO: Write your code below this line
+    # if any ("cookie" in segment["text"] for segment in transcript["segments"]):
+    #     return {"message": "cookie found"}
+    # return {"message": "no cookie found"}
+
+    # print(transcript)
+    # return {"message" : "testing for webhook"}
 
 
+    for segment in transcript["segments"]:
+        print (segment["text"])
+        if "tired" in segment["text"].lower():
+            print("tired found")
+            return {"message" : "take a break"}
 if __name__ == "__main__":
     import uvicorn
 
